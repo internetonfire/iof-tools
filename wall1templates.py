0218fe6068dcc1bbcd4b9f539a9c0a8032ff9f81
Michele Segata <segata@ccs-labs.org>
2019-05-21 19:43:55 +0200

    gen-rspec.py: load ubuntu 18.04 when starting nodes on virtual walls

--- wall1templates.py
+++ wall1templates.py
@@ -17,7 +17,9 @@ footer_template = """</rspec>"""
 node_template = """<node client_id="%s" exclusive="true" \
   component_manager_id="urn:publicid:IDN+wall1.ilabt.iminds.be+authority+cm" \
   component_id="%s">
-  <sliver_type name="raw-pc"/>
+  <sliver_type name="raw-pc">
+    <disk_image name="urn:publicid:IDN+wall2.ilabt.iminds.be+image+emulab-ops:UBUNTU18-64-STD"/>
+  </sliver_type>
   <location xmlns="http://jfed.iminds.be/rspec/ext/jfed/1" x="%f" y="%f"/>
 </node>\n"""
 
