93ccfdde3e9de818ef890e72c221610219acbe51
Michele Segata <segata@ccs-labs.org>
2019-08-14 16:16:19 +0200

    get-node-info.sh: fetch list of available interfaces

--- get-node-info.sh
+++ get-node-info.sh
@@ -2,3 +2,4 @@
 
 ansible nodes -m setup --tree cpu_info -a 'gather_subset=!all,!min,hardware filter=ansible_processor_*'
 ansible nodes -m setup --tree nw_info -a 'gather_subset=!all,!min,network filter=ansible_default_ipv4'
+ansible nodes -m setup --tree nw_if -a 'gather_subset=!all,!min,network filter=ansible_interfaces'
