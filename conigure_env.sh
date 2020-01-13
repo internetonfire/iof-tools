344e481a9548d9328e026002086ee98d0b2fe370
tiamilani <mattia.milani10@gmail.com>
2020-01-13 10:55:13 +0100

    config script updated

--- conigure_env.sh
+++ conigure_env.sh
@@ -2,20 +2,20 @@
 
 apt-get install python python-m2crypto python-dateutil python-openssl libxmlsec1 xmlsec1 libxmlsec1-openssl libxmlsec1-dev autoconf python-pip python3-pip ansible default-jdk
 pip3 install networkx==2.4rc1 jinja2
-cd $HOME
+cd "$HOME" || exit
 mkdir src
-cd src
+cd src || exit
 git clone https://ans.disi.unitn.it/redmine/iof-tools.git
-cd iof-tools
+cd iof-tools || exit
 git checkout reorganizedVersion
-cd $HOME/src
+cd "$HOME"/src || exit
 git clone https://github.com/GENI-NSF/geni-tools omni
-cd omni
+cd omni || exit
 ./autogen.sh
 ./configure
 make
-cd $HOME/src/iof-tools
-ln -s $HOME/src/omni/src/omni omni
+cd "$HOME"/src/iof-tools || exit
+ln -s "$HOME"/src/omni/src/omni omni
 export PATH="$PATH:."
 openssl rsa -in twist-encrypt.key -out twist.key
 openssl x509 -pubkey -noout -in ~/.ssh/twist.cert > ~/.ssh/twist.pub
