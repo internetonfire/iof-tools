1da6f77ca9fa6b80fcf7096eccb4fed1d9d2da13
tiamilani <mattia.milani10@gmail.com>
2020-01-13 14:30:44 +0100

    bug fix

--- configure_env.sh
+++ configure_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install python python-m2crypto python-dateutil python-openssl libxmlsec1 xmlsec1 libxmlsec1-openssl libxmlsec1-dev autoconf python-pip python3-pip ansible default-jdk
-pip3 install networkx==2.4rc1 jinja2
+sudo apt-get -y install python python-m2crypto python-dateutil python-openssl libxmlsec1 xmlsec1 libxmlsec1-openssl libxmlsec1-dev autoconf python-pip python3-pip ansible default-jdk gnuplot-nox
+pip3 install networkx==2.4rc1 jinja2 numpy pandas
 cd "$HOME"/src || exit
 git clone https://github.com/GENI-NSF/geni-tools omni
 cd omni || exit
