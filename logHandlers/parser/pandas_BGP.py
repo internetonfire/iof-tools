5bf71e4764c18b6f28b99bb5bd7339d8ac5e6e21
Leonardo Maccari <leonardo.maccari@unive.it>
2020-02-25 16:17:27 +0100

    now creates also time lists

--- logHandlers/parser/pandas_BGP.py
+++ logHandlers/parser/pandas_BGP.py
@@ -4,32 +4,53 @@ import numpy as np
 import itertools as it
 
 
+samples = 100
+delta = '10ms'
+
 def make_index(t_r_number=3, run_id_number=3, AS_number=5):
     AS_list = ['AS' + str(x) for x in range(AS_number)]
     t_r_list = AS_list[:t_r_number]
     run_id_list = list(range(run_id_number))
     return([t_r_list, run_id_list, AS_list])
 
-def fill_data(names, t_r_list, run_id_list, AS_list, mode='ZERO'):
+def fill_run_table(names, t_r_list, run_id_list, AS_list, mode='ZERO', time_len=0):
+    """ creates a list of dictionaries to fill a multiindex dataframe with
+        dummy data """
     data = []
-    for val in it.product(t_r_list, run_id_list, AS_list):
-        updates = 0
+    time_data = []
+    for idx, val in enumerate(it.product(t_r_list, run_id_list, AS_list)):
+        tot_updates = idx
+        updates = [0] * time_len
         if mode == 'RANDOM':
-            updates = np.random.random()
+            tot_updates = np.random.random()
+            updates = np.random.randn(time_len) + idx
+        if mode == 'LINEAR':
+            tot_updates = idx
+            updates = [idx] * time_len
         data_dict = {}
         for i in range(len(names)): 
             data_dict[names[i]] = str(val[i])
-        data_dict['updates'] =  str(updates)
+        data_dict['tot_updates'] =  str(tot_updates)
         data.append(data_dict)
-    return data
+        time_data.append(updates)
+    return data, np.matrix.transpose(np.array(time_data))
     
+def fill_updates_table(names):
+    pass
+
+
 names=['t_r', 'run_id', 'AS']
 indexes = make_index()
-data_set = fill_data(names, indexes[0], indexes[1], indexes[2])
+data_set, time_data = fill_run_table(names, indexes[0], indexes[1], indexes[2], 
+        mode='LINEAR', time_len=samples)
             
 run_table_index = pd.MultiIndex.from_product(indexes, names=names)
-run_table = pd.DataFrame(data_set, index=run_table_index, columns = ['updates'])
-print(run_table)
+run_table = pd.DataFrame(data_set, index=run_table_index, columns = ['tot_updates'])
+
+update_table_index = pd.timedelta_range(0, periods=samples, freq=delta)
+
+update_table = pd.DataFrame(time_data, index=update_table_index, columns=run_table_index)
+print(update_table)
 
 
 
